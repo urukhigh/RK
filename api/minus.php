4fcaf00886f65e30455015a5d323d8fddf04788d
urukhigh <urukhigh2014@gmail.com>
2021-12-09 20:13:31 +0200

    now includ

--- api/minus.php
+++ api/minus.php
@@ -4,7 +4,8 @@ $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x - $y;
 
-include("C:/AppParams/params.php");
+include(getenv("MYAPP_CONFIG"));
+
 // $conn = mysqli_connect("localhost","root","","calc");
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 $sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonymus')";
