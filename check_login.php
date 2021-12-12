b81ffb8b10ee4fb48066420782ffdd0b3bcde7f1
urukhigh <urukhigh2014@gmail.com>
2021-12-12 15:32:14 +0200

    22

--- check_login.php
+++ check_login.php
@@ -12,28 +12,46 @@
             $user = $_REQUEST["user"];
             $pwd = $_REQUEST["pwd"];
             $hash = hash('sha256',$pwd);
-            $sql = "SELECT ID, UserName FROM users 
-            WHERE UserName=? AND PwdHash=?";
+            // $sql = "SELECT ID, UserName FROM users 
+            // WHERE UserName=? AND PwdHash=?";
+            $sql_select = "SELECT ID, UserName
+            FROM users WHERE UserName='$user'";
+            $sql_insert = "INSERT INTO users
+            SET UserName='$user', PwdHash='$hash'";
             $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
             // Нудьга. Передача параметров в sql выражения.
             // Противодействие sql injection.
-            $statement = mysqli_prepare($conn,$sql);
-            mysqli_stmt_bind_param($statement,"ss",$user,$hash);
-            mysqli_stmt_execute($statement);
-            $cursor = mysqli_stmt_get_result($statement);
-            $result = mysqli_fetch_all($cursor);
-            echo(mysqli_error($conn));
+            // $statement = mysqli_prepare($conn,$sql);
+            // mysqli_stmt_bind_param($statement,"ss",$user,$hash);
+            // mysqli_stmt_execute($statement);
+            // $cursor = mysqli_stmt_get_result($statement);
+            // $result = mysqli_fetch_all($cursor);
+            // echo(mysqli_error($conn));
             //var_dump($result);
-            mysqli_close($conn);
+            $cursor = mysqli_query($conn,$sql_select);
+            $result = mysqli_fetch_all($cursor);
+            echo (mysqli_error($conn));
+            // mysqli_close($conn);
 
-            if (count($result) > 0) {
-                echo ("<h1>Hello, $user!</h1>");
-                $_SESSION["user"] = $user;
-                echo('<meta http-equiv="refresh" content="2; URL=calc.php">');
+            if (!empty($result['id'])) {
+                // echo ("<h1>Hello, $user!</h1>");
+                // $_SESSION["user"] = $user;
+                // echo('<meta http-equiv="refresh" content="2; URL=calc.php">');
+                exit("Логин существует");
+            }
+            // else {
+                // echo ("<h2>BAD LOGIN!</h2>");
+            // }
+            $conn1 = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
+            $result1 = mysqli_query($conn1,$sql_insert);
+                
+            if ($result1 == 'True') {
+                echo("Регистрация успешна <a href='index_.html'>На центральный портал</a>");
             }
             else {
-                echo ("<h2>BAD LOGIN!</h2>");
+                echo("Ошибка в регистрации!");
             }
+        
         ?>
     </body>
 </html>
